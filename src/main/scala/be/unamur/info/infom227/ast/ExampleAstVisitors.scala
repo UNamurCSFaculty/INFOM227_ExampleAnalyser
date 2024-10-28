c4d4b1a1f26eadf2c636cb8f9060ea111a2cfc06
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-10-28 22:03:18 +0100

    Rename environment to data

--- src/main/scala/be/unamur/info/infom227/ast/ExampleAstVisitors.scala
+++ src/main/scala/be/unamur/info/infom227/ast/ExampleAstVisitors.scala
@@ -2,52 +2,52 @@ package be.unamur.info.infom227.ast
 
 import be.unamur.info.infom227.*
 
-trait ExampleSequenceVisitor[T, E] {
-  def visitExampleProgram(node: ExampleProgram, environment: E): T
+trait ExampleSequenceVisitor[T, D] {
+  def visitExampleProgram(node: ExampleProgram, data: D): T
 
-  def visitExampleScope(node: ExampleScope, environment: E): T
+  def visitExampleScope(node: ExampleScope, data: D): T
 
-  def visitExampleStatements(node: ExampleStatements, environment: E): T
+  def visitExampleStatements(node: ExampleStatements, data: D): T
 }
 
-trait ExampleStatementVisitor[T, E] {
-  def visitExampleDeclareStatement(node: ExampleDeclareStatement, environment: E): T
+trait ExampleStatementVisitor[T, D] {
+  def visitExampleDeclareStatement(node: ExampleDeclareStatement, data: D): T
 
-  def visitExampleAssignStatement(node: ExampleAssignStatement, environment: E): T
+  def visitExampleAssignStatement(node: ExampleAssignStatement, data: D): T
 
-  def visitExamplePrintStatement(node: ExamplePrintStatement, environment: E): T
+  def visitExamplePrintStatement(node: ExamplePrintStatement, data: D): T
 
-  def visitExampleIfStatement(node: ExampleIfStatement, environment: E): T
+  def visitExampleIfStatement(node: ExampleIfStatement, data: D): T
 
-  def visitExampleWhileStatement(node: ExampleWhileStatement, environment: E): T
+  def visitExampleWhileStatement(node: ExampleWhileStatement, data: D): T
 }
 
-trait ExampleTypeVisitor[T, E] {
-  def visitExampleInt(node: ExampleInt.type, environment: E): T
+trait ExampleTypeVisitor[T, D] {
+  def visitExampleInt(node: ExampleInt.type, data: D): T
 
-  def visitExampleBool(node: ExampleBool.type, environment: E): T
+  def visitExampleBool(node: ExampleBool.type, data: D): T
 }
 
-trait ExampleExpressionVisitor[T, E] {
-  def visitExampleIntegerConstant(node: ExampleIntegerConstant, environment: E): T
+trait ExampleExpressionVisitor[T, D] {
+  def visitExampleIntegerConstant(node: ExampleIntegerConstant, data: D): T
 
-  def visitExampleIntegerVariable(node: ExampleIntegerVariable, environment: E): T
+  def visitExampleIntegerVariable(node: ExampleIntegerVariable, data: D): T
 
-  def visitExampleIntegerUnaryOperation(node: ExampleIntegerUnaryOperation, environment: E): T
+  def visitExampleIntegerUnaryOperation(node: ExampleIntegerUnaryOperation, data: D): T
 
-  def visitExampleIntegerBinaryOperation(node: ExampleIntegerBinaryOperation, environment: E): T
+  def visitExampleIntegerBinaryOperation(node: ExampleIntegerBinaryOperation, data: D): T
 
-  def visitExampleBooleanConstant(node: ExampleBooleanConstant, environment: E): T
+  def visitExampleBooleanConstant(node: ExampleBooleanConstant, data: D): T
 
-  def visitExampleBooleanVariable(node: ExampleBooleanVariable, environment: E): T
+  def visitExampleBooleanVariable(node: ExampleBooleanVariable, data: D): T
 
-  def visitExampleBooleanUnaryOperation(node: ExampleBooleanUnaryOperation, environment: E): T
+  def visitExampleBooleanUnaryOperation(node: ExampleBooleanUnaryOperation, data: D): T
 
-  def visitExampleBooleanBinaryOperation(node: ExampleBooleanBinaryOperation, environment: E): T
+  def visitExampleBooleanBinaryOperation(node: ExampleBooleanBinaryOperation, data: D): T
 
-  def visitExampleBooleanEqualComparisonOperation(node: ExampleBooleanEqualComparisonOperation, environment: E): T
+  def visitExampleBooleanEqualComparisonOperation(node: ExampleBooleanEqualComparisonOperation, data: D): T
 
-  def visitExampleIntegerComparisonOperation(node: ExampleIntegerComparisonOperation, environment: E): T
+  def visitExampleIntegerComparisonOperation(node: ExampleIntegerComparisonOperation, data: D): T
 
-  def visitExampleIntegerEqualComparisonOperation(node: ExampleIntegerEqualComparisonOperation, environment: E): T
+  def visitExampleIntegerEqualComparisonOperation(node: ExampleIntegerEqualComparisonOperation, data: D): T
 }
