cdd07e8bd35c668f8073b23a38c2addcfecab455
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2025-10-01 23:42:09 +0200

    Add exit code in Main

--- src/main/scala/be/unamur/info/infom227/Main.scala
+++ src/main/scala/be/unamur/info/infom227/Main.scala
@@ -9,6 +9,10 @@ import org.antlr.v4.runtime.CharStreams
 
 import scala.util.{Try, Success, Failure}
 
+val SUCCESS_ERROR_CODE = 0
+val COMPILATION_ERROR_CODE = 1
+val FATAL_ERROR_CODE = 2
+val UNKNOWN_ACTION_ERROR_CODE = 3
 
 @main def main(action: String, file: String): Unit = {
   action match {
@@ -20,10 +24,15 @@ import scala.util.{Try, Success, Failure}
         result <- ExampleInterpreter.run(ast)
       } yield result
 
-      tryResult.recover {
-        case exception: CannotBuildAstException => println(s"Compilation Error:\n${exception.getMessage}")
-        case exception: Throwable => println(s"Fatal error:\n${exception.getMessage}")
-      }
+      tryResult match
+        case Success(messages) =>
+          System.exit(SUCCESS_ERROR_CODE)
+        case Failure(exception: CannotBuildAstException) =>
+          println(s"Compilation Error:\n${exception.getMessage}")
+          System.exit(COMPILATION_ERROR_CODE)
+        case Failure(exception: Throwable) =>
+          println(s"Fatal error:\n${exception.getMessage}")
+          System.exit(FATAL_ERROR_CODE)
     case "zero-analysis" =>
       val tryMessages = for {
         charStream <- Try(CharStreams.fromFileName(file))
@@ -39,8 +48,15 @@ import scala.util.{Try, Success, Failure}
           for ((lineNumber, messageType, message) <- messages) {
             println(s"[$messageType] Line $lineNumber : $message")
           }
-        case Failure(exception: CannotBuildAstException) => println(s"Compilation Error:\n${exception.getMessage}")
-        case Failure(exception: Throwable) => println(s"Fatal error:\n${exception.getMessage}")
-    case action => println(f"Unknown action: $action")
+          System.exit(SUCCESS_ERROR_CODE)
+        case Failure(exception: CannotBuildAstException) =>
+          println(s"Compilation Error:\n${exception.getMessage}")
+          System.exit(COMPILATION_ERROR_CODE)
+        case Failure(exception: Throwable) =>
+          println(s"Fatal error:\n${exception.getMessage}")
+          System.exit(FATAL_ERROR_CODE)
+    case action =>
+      println(f"Unknown action: $action")
+      System.exit(UNKNOWN_ACTION_ERROR_CODE)
   }
 }
